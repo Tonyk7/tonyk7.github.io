e14276acc500e5d0dbd62b5753a2a1d77628a6c4
Tony <26528179+Tonyk7@users.noreply.github.com>
2018-10-11 17:31:15 -0400

    add tweak(s)

--- update.sh
+++ update.sh
@@ -4,9 +4,9 @@ sed -i -e '/^SHA/d' ./Packages
 bzip2 -c9k ./Packages > ./Packages.bz2
 printf "Origin: Tonyk7's Repo\nLabel: Tonyk7\nSuite: stable\nVersion: 1.0\nCodename: Tonyk7\nArchitecture: iphoneos-arm\nComponents: main\nDescription: Tonyk7's Tweaks\nmd5sum:\n "$(cat ./Packages | md5 -r | cut -d ' ' -f 1)" "$(stat -f%z Packages)" Packages\n "$(cat ./Packages.bz2 | md5 -r | cut -d ' ' -f 1)" "$(stat -f%z Packages.bz2)" Packages.bz2\n" >Release;
 python3 generate_packageinfo.py
-printf "Updating 'latest tweaks' file"
+printf "\nUpdating 'latest tweaks' file"
 bash last_updates.sh
-printf "Pushing to github..."
+printf "\nPushing to github..."
 git add *
 git commit -m "add tweak(s)"
 git push -u origin master
